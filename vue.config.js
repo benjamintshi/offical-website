47fc2e123aebae5765c4d5f49417b878eb60b6d1
liuhui7821037 <286135703@qq.com>
2019-12-01 11:19:28 +0800

    p

--- vue.config.js
+++ vue.config.js
@@ -131,17 +131,7 @@ module.exports = {
 
     open: true,
 
-    proxy: {
-      // '/api': {     //这里最好有一个 /
-      //   target: 'http://cultrual.bbwhm.com:8087/api',  // 后台接口域名
-      //   ws: true,        //如果要代理 websockets，配置这个参数
-      //   secure: false,  // 如果是https接口，需要配置这个参数
-      //   changeOrigin: true,  //是否跨域
-      //   pathRewrite:{
-      //     '^/api':''
-      //   }
-      // }
-    }
+    proxy: null
 
   },
   lintOnSave: true,
