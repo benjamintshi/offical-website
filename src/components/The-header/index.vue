93deae90b844fc9cb0136201fdd789f5f83195f8
吴静 <861850257@qq.com>
2019-12-02 15:54:21 +0800

    bug修复

--- src/components/The-header/index.vue
+++ src/components/The-header/index.vue
@@ -36,8 +36,9 @@
         <div class="top-logo">
           <img class="top-logo-bg" src="static/images/top/top-bg.png" alt="">
           <ul>
-
+            <img   style="width: 342px;" src="static/images/top/logo2.png" alt="">
             <p>
+
               <span>
                 奉献
               </span>
