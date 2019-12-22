c969c1b096f5aa156c9ce1439e115024b28040c2
caozq <caoke_001@163.com>
2019-12-22 15:14:38 +0800

    活动详情开发完毕，记得传参数id=11，画面才能正常显示

--- mobile/custom-js/common.local.js
+++ mobile/custom-js/common.local.js
@@ -1,3 +1,8 @@
+/**
+ * 项目日期格式化，将'2019-12-11 11:12:22'，格式化为''2019.12.11'
+ * @param date_str
+ * @returns {string}
+ */
 function date_format(date_str) {
     if (empty(date_str)) {
         return '';
