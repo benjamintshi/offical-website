4961a7a9b5d96f361705aa6e0e0b969fd1120a73
caozq <caoke_001@163.com>
2019-12-25 14:19:59 +0800

    生成登陆二维码，定时检测登陆状态

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,37 +1,82 @@
 import util from '../../utils/postRequest.js';
 import axios from "axios";
+import {ajax_get, ajax_post} from '../../utils/axios.util';
+import constant from '../../utils/constant'
+import {md5} from "../../utils/common";
 
 
 export default {
   data() {
     return {
-      login:{
-        account:"",
-        passwd:""
+      login: {
+        account: "",
+        passwd: ""
       },
       errInfo: '',
-      loginByAccount:true
+      loginByAccount: true,
+      qr_code_src: './static/images/scan.png',
+      check_qr_code_for_login_timer: null
     }
   },
-  mounted(){
+  mounted() {
 
   },
-  methods:{
-   /* toLogin(){
-      var url = "http://zyz.liyue.com/socket/api/vUser/appLogin";
-      util.post(url,this.login)
-        .then(response => {
-          if(response.data.data.userId != null){
-            window.location.href = 'http://zyz.liyue.com/view';
-          }else{
-            this.errInfo='账号或密码错误'
+  watch: {
+    loginByAccount(val, oldVal) {
+      if (val === false) {//生成登陆用二维码
+        this.gen_qr_code_for_login();
+      } else {//取消定时接口调用
+        if (this.check_qr_code_for_login_timer != null) {
+          clearTimeout(this.check_qr_code_for_login_timer);
+        }
+      }
+    }
+  },
+  methods: {
+    check_qr_code_for_login() {
+      const that = this;
+      ajax_get(constant.api_base_url + '/vQrCodeLogin/checkLoginStatus', null, function (data) {
+        if (data.code !== '200') {
+          if (data.message === '请先进行PC登陆二维码生成') {
+            alert(data.message);
+          } else {
+            that.check_qr_code_for_login_timer = setTimeout(that.check_qr_code_for_login, 3000);// 继续检测
           }
-      })
-        .catch(function (error) {
-          console.log(error);
-        });
-    },*/
-    toLogin(){
+          return;
+        }
+        data = data.data;
+        if (data.loginStatus !== 1) {
+          alert(data.loginMsg);
+          return;
+        }
+        window.location.href = './';// 跳转到首页
+      });
+    },
+    gen_qr_code(content) {
+      const that = this;
+      ajax_post(constant.api_base_url + '/qrCode/genQRCodeBase64', {content: content, width: 200, height: 200}, function (data) {
+        that.qr_code_src = data;
+        if (that.check_qr_code_for_login_timer != null) {
+          clearTimeout(that.check_qr_code_for_login_timer);
+        }
+        that.check_qr_code_for_login_timer = setTimeout(that.check_qr_code_for_login, 3000);// 继续检测
+      });
+    },
+    gen_qr_code_for_login() {
+      const that = this;
+      ajax_get(constant.api_base_url + '/vQrCodeLogin/getQrCodeForLogin', null, function (data) {
+        if (data.code !== '200') {
+          alert(data.message);
+          return;
+        }
+        const qr_str = JSON.stringify(data.data);
+        that.gen_qr_code(qr_str);
+      });
+    },
+    changeLoginByAccount(isLoginByAccount) {
+      this.loginByAccount = isLoginByAccount;
+    },
+    toLogin() {
       axios({
         method: 'post',
         url: 'http://zyz.liyue.com/socket/api/vUser/appLogin',
@@ -44,10 +89,10 @@ export default {
       })
         .then(response => {
           console.log(response);
-          if(response.data.data.userId != null){
+          if (response.data.data.userId != null) {
             window.location.href = 'http://zyz.liyue.com/view';
-          }else{
-            this.errInfo='账号或密码错误'
+          } else {
+            this.errInfo = '账号或密码错误'
           }
         })
         .catch(function (error) {
