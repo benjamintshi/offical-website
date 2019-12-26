ce949f29e23e7c7ba1f863bff71b494427ea690a
leizc <2369648034@qq.com>
2019-12-26 16:52:51 +0800

    检索画面开发 完成

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -13,7 +13,7 @@
           <p>
             可能感兴趣：
             <span v-for="(item,index) in adviceList" :key="index"
-                  class="a-hover-style" @click="inputValue = item">{{item}}
+                  class="a-hover-style" @click="searchByHotWord(item.name)" >{{item.name}}
             </span>
           </p>
         </div>
@@ -22,22 +22,62 @@
     </ul>
     <ul class="search-result">
       <li class="nav">
-        <div v-if="seachValue">
-          <p>关于“<span class="tag">{{seachValue}}</span>”的搜索结果 </p>
+        <div>
+          <p>关于“<span class="tag">{{searchValue}}</span>”的搜索结果 </p>
           <p class="menu">
-            <span v-for="(item,index) in menu" :key="index" :class="activeMenu == item.id?'active':''" @click="swtichMenu(item)">
+            <span v-for="(item,index) in menu" :key="index" :class="activeMenu == item.id?'active':''"   @click="switchMenu(item)">
                 {{item.name}}
             </span>
           </p>
         </div>
 
       </li>
-      <li class="search-result-content">
-        <div v-if="resultList.length>0">
+      <li class="search-result-content" v-show="activeMenu === 'a'">
+        <div v-if="newsList.length > 0">
           <Row >
-            <i-col span="6" v-for="(item,index) in resultList" :key="index">
-              <img :src="item.img" alt="">
-              <p>{{item.title}}</p>
+            <i-col span="6" v-for="(item,index) in newsList" :key="index">
+              <div @click="toNewsDetail(item)">
+              <img :src="item.newsCover" alt="">
+              <p>{{item.newsTitle}}</p>
+              </div>
+            </i-col>
+          </Row>
+          <Page v-if="total>12" :total="total" :pageSize="pageSize" prev-text="上一页" next-text="下一页" :current="pageNum" @on-change="changePage"/>
+
+        </div>
+        <div class="empty" v-else>
+          <img src="static/images/common/empty.png" alt="">
+          <p>暂无相关内容</p>
+        </div>
+      </li>
+
+      <li class="search-result-content" v-show="activeMenu === 'b'">
+        <div v-if="activityList.length>0">
+          <Row >
+            <i-col span="6" v-for="(item,index) in activityList" :key="index">
+              <div @click="toActivityDetail(item)">
+              <img :src="item.activityCover" alt="">
+              <p>{{item.activityName}}</p>
+              </div>
+            </i-col>
+          </Row>
+          <Page v-if="total>12" :total="total" :pageSize="pageSize" prev-text="上一页" next-text="下一页" :current="pageNum" @on-change="changePage"/>
+
+        </div>
+        <div class="empty" v-else>
+          <img src="static/images/common/empty.png" alt="">
+          <p>暂无相关内容</p>
+        </div>
+      </li>
+
+      <li class="search-result-content" v-show="activeMenu === 'c'">
+        <div v-if="teamList.length > 0">
+          <Row >
+            <i-col span="6" v-for="(item,index) in teamList" :key="index">
+              <div @click="toTeamDetail(item)">
+                <img :src="item.teamLogo" alt="">
+                <p>{{item.teamName}}</p>
+              </div>
             </i-col>
           </Row>
           <Page v-if="total>12" :total="total" :pageSize="pageSize" prev-text="上一页" next-text="下一页" :current="pageNum" @on-change="changePage"/>
